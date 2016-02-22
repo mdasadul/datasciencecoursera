8252deff332de8d1a6f146ebc29acaacdc45be92
mdasadul <mdasadul@ualberta.ca>
2016-02-22 15:42:03 -0600

    adding more week 4 lecture

--- data-cleaning/week4.R
+++ data-cleaning/week4.R
@@ -1,3 +1,8 @@
 cameradata<-read.csv("./data/cameras.cav")
 names(cameradata)
 tolower(names(cameradata))
+splitNames =strsplit(names(cameradata),"\\.")
+splitNames[[6]]
+
+mylist<-list(letters("A","B","C"),numbers=1:3,matrix(1:25,ncol=5))
+head(mylist)
